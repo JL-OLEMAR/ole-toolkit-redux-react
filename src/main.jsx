5d66832e41bcb5da182628bae9b7cf88169e33f7
JL-OLEMAR <joseluis19963@gmail.com>
2022-08-05 23:59:37 -0500

    feat: add RTK Query

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 // import { App } from './App.jsx'
-import { PokemonApp } from './PokemonApp.jsx'
+// import { PokemonApp } from './PokemonApp.jsx'
+import { TodoApp } from './TodoApp.jsx'
 import { store } from './store'
 import './index.css'
 
@@ -11,7 +12,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp />
     </Provider>
   </React.StrictMode>
 )
